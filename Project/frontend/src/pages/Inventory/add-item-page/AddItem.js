a61cf751cdcef6600c9dc518525283a231e5bb45
IT21189630 <it21189630@my.sliit.lk>
2023-04-19 20:34:12 +0530

    dashboard state changing bug fixed

--- Project/frontend/src/pages/Inventory/add-item-page/AddItem.js
+++ Project/frontend/src/pages/Inventory/add-item-page/AddItem.js
@@ -1,132 +1,210 @@
-import React, { useState } from 'react'
-import swal from 'sweetalert2'
-import AdminLayout from '../../Layouts/AdminLayout'
-import api from '../../../services/api'
-import './addItem.scss'
+import React, { useState } from "react";
+import swal from "sweetalert2";
+import AdminLayout from "../../Layouts/AdminLayout";
+import api from "../../../services/api";
+import "./addItem.scss";
 
 const AddItem = () => {
+	const [formData, setFormData] = useState({
+		sku: "",
+		itemName: "",
+		category: "",
+		price: "",
+		rackNo: "",
+		quantity: "",
+		manufacturer: "",
+		productDescription: "",
+		productImage: "",
+	});
 
-   const [formData , setFormData] = useState({
-      sku:"",
-      itemName:"",
-      category:"",
-      price:"",
-      rackNo:"",
-      quantity:"",
-      manufacturer:"",
-      productDescription:"",
-      productImage:""
-  })
+	const addItemFormHandler = (event) => {
+		event.preventDefault();
+		console.log(formData);
 
-  const addItemFormHandler = (event) => {
-    event.preventDefault()
-    console.log(formData);
+		if (formData.sku !== "" && formData.category !== "") {
+			api.post("/", formData)
+				.then((response) => {
+					console.log(response);
+					swal.fire({
+						icon: "success",
+						iconColor: "#7D5FFF",
+						title: "Operation Success",
+						text: "Item added to the inventory!",
+					});
+					console.log(response);
+				})
+				.catch((error) => {
+					console.log(error);
+				});
 
-    if(formData.sku !== "" && formData.category !== ""){
-        api.post('/',formData).then(response => {
-        
-        console.log(response);
-        swal.fire({
-          icon: 'success',
-          iconColor: '#7D5FFF',
-          title: 'Operation Success',
-          text: 'Item added to the inventory!',
-        })
-        console.log(response);
-      })
-      .catch(error => {
-        console.log(error);
-      })
+			setFormData({
+				sku: "",
+				itemName: "",
+				category: "",
+				price: "",
+				rackNo: "",
+				quantity: "",
+				manufacturer: "",
+				productDescription: "",
+				productImage: "",
+			});
+		} else {
+			swal.fire({
+				icon: "error",
+				iconColor: "#e74c3c",
+				title: "Operation Not Success",
+				text: "fill the relevant fields first",
+			});
+		}
+	};
 
-      setFormData({
-        sku:"",
-        itemName:"",
-        category:"",
-        price:"",
-        rackNo:"",
-        quantity:"",
-        manufacturer:"",
-        productDescription:"",
-        productImage:""})
-      }
+	const addItemInputHandler = (event) => {
+		setFormData({ ...formData, [event.target.name]: event.target.value });
+	};
 
-      else{
-        swal.fire({
-          icon: 'error',
-          iconColor: '#e74c3c',
-          title: 'Operation Not Success',
-          text: 'fill the relevant fields first',
-        })
-      }
-}
+	return (
+		<AdminLayout>
+			<div className="add-item-container-main">
+				{/* this is the form container */}
+				<form
+					className="add-item-form-container"
+					onSubmit={addItemFormHandler}
+				>
+					<span className="tagline-add-item">
+						Fill the form for add item
+					</span>
+					{/* input field container */}
+					<div className="column-container">
+						{/* column one */}
+						<div className="add-item-column">
+							<section className="input-container">
+								<span className="input-title">item name</span>
+								<input
+									className="input-field"
+									value={formData.itemName}
+									name="itemName"
+									onChange={addItemInputHandler}
+								/>
+							</section>
+							<section className="input-container">
+								<span className="input-title">
+									store keeping unit (SKU)
+								</span>
+								<input
+									className="input-field"
+									value={formData.sku}
+									name="sku"
+									onChange={addItemInputHandler}
+								/>
+							</section>
+							<section className="input-container">
+								<span className="input-title">category</span>
+								<select
+									className="input-field"
+									name="category"
+									value={formData.category}
+									onChange={addItemInputHandler}
+								>
+									<option
+										className="select-option"
+										value="undefined"
+									></option>
+									<option
+										className="select-option"
+										value="clinical-item"
+									>
+										Clinical Item
+									</option>
+									<option
+										className="select-option"
+										value="store-item"
+									>
+										Pet Store Item
+									</option>
+								</select>
+							</section>
+							<section className="input-container">
+								<span className="input-title">unit price</span>
+								<input
+									className="input-field"
+									value={formData.price}
+									name="price"
+									onChange={addItemInputHandler}
+								/>
+							</section>
+							<section className="input-container">
+								<span className="input-title">rack number</span>
+								<input
+									className="input-field"
+									value={formData.rackNo}
+									name="rackNo"
+									onChange={addItemInputHandler}
+								/>
+							</section>
+						</div>
+						{/* column two */}
+						<div className="add-item-column">
+							<section className="input-container">
+								<span className="input-title">quantity</span>
+								<input
+									className="input-field"
+									value={formData.quantity}
+									name="quantity"
+									onChange={addItemInputHandler}
+								/>
+							</section>
+							<section className="input-container">
+								<span className="input-title">
+									manufacturer
+								</span>
+								<input
+									className="input-field"
+									value={formData.manufacturer}
+									name="manufacturer"
+									onChange={addItemInputHandler}
+								/>
+							</section>
+							<section className="input-container">
+								<span className="input-title">
+									product description
+								</span>
+								<textarea
+									className="input-textarea"
+									value={formData.productDescription}
+									id=""
+									cols="30"
+									rows="10"
+									name="productDescription"
+									onChange={addItemInputHandler}
+								></textarea>
+							</section>
+							<section className="input-container">
+								<span className="input-title">
+									product image
+								</span>
+								<input
+									type="file"
+									name="productImage"
+									value={formData.productImage}
+									id=""
+									className="input-field"
+									onChange={addItemInputHandler}
+								/>
+							</section>
+							<div className="btn-container-add-item">
+								<button type="submit" className="submit-btn">
+									Submit
+								</button>
+								<button type="reset" className="reset-btn">
+									Reset
+								</button>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+		</AdminLayout>
+	);
+};
 
-const addItemInputHandler = (event) => {
-  setFormData({...formData , [event.target.name]: event.target.value})
-}
-
-  return (
-    <AdminLayout>
-    <div className="add-item-container-main">
-        {/* this is the form container */}
-        <form className="add-item-form-container" onSubmit={addItemFormHandler}>
-            <span className="tagline-add-item">Fill the form for add item</span>
-            {/* input field container */}
-            <div className="column-container">
-              {/* column one */}
-              <div className="add-item-column">
-                <section className="input-container">
-                  <span className="input-title">item name</span>
-                  <input className="input-field" value={formData.itemName} name="itemName" onChange={addItemInputHandler}/>
-                </section>
-                <section className="input-container">
-                  <span className="input-title">store keeping unit (SKU)</span>
-                  <input className="input-field" value={formData.sku} name="sku" onChange={addItemInputHandler}/>
-                </section>
-                <section className="input-container">
-                  <span className="input-title">category</span>
-                  <select className="input-field" name="category" value={formData.category} onChange={addItemInputHandler}>
-                      <option className='select-option' value="undefined"></option>
-                      <option className='select-option' value="clinical-item">Clinical Item</option>
-                      <option className='select-option' value="store-item">Pet Store Item</option>
-                  </select> 
-                </section>
-                <section className="input-container">
-                  <span className="input-title">unit price</span>
-                  <input className="input-field" value={formData.price} name="price" onChange={addItemInputHandler}/>
-                </section>
-                <section className="input-container">
-                  <span className="input-title">rack number</span>
-                  <input className="input-field" value={formData.rackNo} name="rackNo" onChange={addItemInputHandler}/>
-                </section>
-              </div>
-              {/* column two */}
-              <div className="add-item-column">
-                    <section className="input-container">
-                        <span className="input-title">quantity</span>
-                        <input className="input-field" value={formData.quantity} name="quantity" onChange={addItemInputHandler}/>
-                    </section>
-                    <section className="input-container">
-                        <span className="input-title">manufacturer</span>
-                        <input className="input-field" value={formData.manufacturer} name="manufacturer" onChange={addItemInputHandler}/>
-                    </section>
-                    <section className="input-container">
-                        <span className="input-title">product description</span>
-                        <textarea className='input-textarea' value={formData.productDescription} id="" cols="30" rows="10" name="productDescription" onChange={addItemInputHandler}></textarea>
-                    </section>
-                    <section className="input-container">
-                        <span className="input-title">product image</span>
-                        <input type="file" name="productImage" value={formData.productImage} id="" className='input-field' onChange={addItemInputHandler}/>
-                    </section>
-                    <div className="btn-container-add-item">
-                      <button type='submit' className="submit-btn">Submit</button>
-                      <button type='reset' className="reset-btn">Reset</button>
-                    </div>
-              </div>
-            </div>
-        </form>
-    </div>
-    </AdminLayout>
-  )
-}
-
-export default AddItem
+export default AddItem;
--- Project/frontend/src/pages/Inventory/manage-inventory-page/manage-inventory.js
+++ Project/frontend/src/pages/Inventory/manage-inventory-page/manage-inventory.js
@@ -1,82 +1,108 @@
-import React, { useState} from 'react'
-import {ImSearch} from 'react-icons/im'
-import ResultContainer from './item-search-result-container'
-import NoItemsDisplayer from './item-empty-result-diplayer'
-import AdminLayout from '../../Layouts/AdminLayout'
-import './manage-inventory.scss'
-import api from '../../../services/api'
+import React, { useEffect, useState } from "react";
+import { ImSearch } from "react-icons/im";
+import ResultContainer from "./item-search-result-container";
+import NoItemsDisplayer from "./item-empty-result-diplayer";
+import AdminLayout from "../../Layouts/AdminLayout";
+import "./manage-inventory.scss";
+import api from "../../../services/api";
 
 function ManageInventoryComponent() {
+	const [inventory, setInventory] = useState([]);
 
-  const [inventory , setInventory] = useState([])
+	const [searchPrompt, setSearchPrompt] = useState("");
 
-  const [searchPrompt , setSearchPrompt] = useState('')
+	useEffect(() => {
+		api.get("/").then((response) => {
+			setInventory(response.data);
+		});
+	}, []);
 
-  const searchFieldHandler = (e) => {
-    setSearchPrompt(e.target.value);
-  }
+	const searchFieldHandler = (e) => {
+		setSearchPrompt(e.target.value);
+	};
 
-  const searchFunction = () => {
-    api.get(`/${searchPrompt}`)
-    .then((response)=>{
-      if(response.status === 200){
-        console.log(response);
-        setInventory(response.data)
-      }
-      else{
-        console.log("no such item");
-        setInventory([])
-      }
-    })
-    .catch((error)=>{
-      console.log("no such item");
-      console.log(error);
-    })
-  }
+	const searchFunction = () => {
+		api.get(`/${searchPrompt}`)
+			.then((response) => {
+				if (response.status === 200) {
+					console.log(response);
+					setInventory(response.data);
+				} else {
+					console.log("no such item");
+					setInventory([]);
+				}
+			})
+			.catch((error) => {
+				console.log("no such item in here bro fucking error");
+				setInventory([]);
+				console.log(error);
+			});
+	};
 
+	const searchFormHandler = (e) => {
+		e.preventDefault();
+		console.log(searchPrompt);
+		searchFunction();
+		setSearchPrompt("");
+	};
 
-  const searchFormHandler = (e) => {
-      e.preventDefault()
-      console.log(searchPrompt);
-      searchFunction()
-      setSearchPrompt("")
-  }
+	return (
+		<AdminLayout>
+			<div className="actionbar-container-manage-inventory">
+				{/* main headline */}
+				{/*Search bar*/}
+				<div className="search-bar-container">
+					<input
+						type="text"
+						className="search-field"
+						placeholder="Search by SKU ID"
+						value={searchPrompt}
+						onChange={searchFieldHandler}
+					/>
+					<form onSubmit={searchFormHandler}>
+						<button type="submit" className="search-btn">
+							<ImSearch />
+						</button>
+					</form>
+				</div>
 
-  return (
-    <AdminLayout>
-    <div className='actionbar-container-manage-inventory'>
-        {/* main headline */}
-        {/*Search bar*/}
-        <div className="search-bar-container">
-          <input type="text" className="search-field" placeholder='Search by store keeping unit..' value={searchPrompt} onChange={searchFieldHandler}/>
-          <form onSubmit={searchFormHandler}>
-              <button type='submit' className="search-btn">
-                <ImSearch/>
-              </button>
-          </form>
-        </div>
+				{/* data fetching section including buttons*/}
 
-        {/* data fetching section including buttons*/}
-        
-        <div className="search-results-section">
-            {/* table headings */}
-            <div className="inventory-info-item-head">
-                  <span className="item-field-head-manage-inventory">Item Name</span>
-                  <span className="item-field-head-manage-inventory">SKU ID</span>
-                  <span className="item-field-head-manage-inventory">Category</span>
-                  <span className="item-field-head-manage-inventory">Manufacturer</span>
-                  <span className="item-field-head-manage-inventory">Quantity</span>
-                  <span className="item-field-head-manage-inventory">Actions</span>
-            </div>
-            {/* scrollable section */}
-             <div className="search-results-container">
-                  {/* display the results */}
-                  {inventory.length === 0 ? <NoItemsDisplayer/> : <ResultContainer inventory={inventory}/>}
-            </div>
-        </div>
-    </div>
-    </AdminLayout>
-  )
+				<div className="search-results-section">
+					{/* table headings */}
+					<div className="inventory-info-item-head">
+						<span className="item-field-head-manage-inventory">
+							Item Name
+						</span>
+						<span className="item-field-head-manage-inventory">
+							SKU ID
+						</span>
+						<span className="item-field-head-manage-inventory">
+							Category
+						</span>
+						<span className="item-field-head-manage-inventory">
+							Manufacturer
+						</span>
+						<span className="item-field-head-manage-inventory">
+							Quantity
+						</span>
+						<span className="item-field-head-manage-inventory">
+							Actions
+						</span>
+					</div>
+					{/* scrollable section */}
+					<div className="search-results-container">
+						{/* display the results */}
+						{inventory.length === 0 ? (
+							<NoItemsDisplayer />
+						) : (
+							<ResultContainer inventory={inventory} />
+						)}
+					</div>
+				</div>
+			</div>
+		</AdminLayout>
+	);
 }
 
-export default ManageInventoryComponent
+export default ManageInventoryComponent;
