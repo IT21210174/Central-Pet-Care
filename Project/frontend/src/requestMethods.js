bf2202d9fc581c97ce10a673551e15974b8815ca
IT21267772 <it21267772@my.sliit.lk>
2023-05-19 04:11:08 +0530

    Updated user request

--- Project/frontend/src/requestMethods.js
+++ Project/frontend/src/requestMethods.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:4000/api/";
 
-const token = JSON.parse(localStorage.getItem("user")).token
+const user = JSON.parse(localStorage.getItem("user"));
+const token = user && user.token;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
